f21ce43a4c7539f24f76bb1b569f5994eef658ca
Jon Pretty <jon.pretty@propensive.com>
2014-10-01 16:06:33 +0200

    Version/dependency updates

--- project/project.scala
+++ project/project.scala
@@ -5,9 +5,8 @@ object project extends ProjectSettings {
   def description = "Rapture Crypto provides a variety of convenient cryptographic methods for use in Scala."
   
   def dependencies = Seq(
-    "core" -> "0.10.0",
-    "codec" -> "0.10.0",
-    "io" -> "0.10.0"
+    "io" -> "0.10.0",
+    "codec" -> "1.0.0"
   )
   
   def thirdPartyDependencies = Nil
